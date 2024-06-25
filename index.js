130b7f24432d37e2a266a7ca0d63f108274d49f7
Altinok Darici <altinokd@microsoft.com>
2024-06-25 15:26:09 -0700

    fix

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-const dpapi = require('bindings')('node-dpapi');
+const fs = require('fs');
+
+const unsupportedDpapi = {
+    protectData() {
+        throw new Error("Dpapi is not supported on this platform");
+    },
+    unprotectData() {
+        throw new Error("Dpapi is not supported on this platform");
+    }
+}
+
+function getDpapi() {
+    if (process.platform !== "win32") {
+        return unsupportedDpapi;
+    }
+
+    const fileName = 'node-dpapi.node';
+
+    const localBuildPath = path.join(__dirname, 'build', 'Release', fileName)
+    if (fs.existsSync(localBuildPath)) {
+        return require(localBuildPath);
+    }
+
+    const arch = process.arch;
+    switch (arch) {
+        case 'arm64':
+        case 'x64':
+        case 'ia32':
+            return require(path.join(__dirname, 'bin', arch, fileName));
+        default:
+            throw unsupportedDpapi;
+    }
+}
+
+const dpapi = getDpapi();
 
 module.exports.protectData = dpapi.protectData;
 module.exports.unprotectData = dpapi.unprotectData;
