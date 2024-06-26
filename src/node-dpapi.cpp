41be286edc8e28d4e930866d78bc2de11f1795a2
Altinok Darici <altinokd@microsoft.com>
2024-06-25 18:52:25 -0700

    fix

--- src/node-dpapi.cpp
+++ src/node-dpapi.cpp
@@ -13,19 +13,19 @@ void ProtectDataCommon(bool protect, const Napi::CallbackInfo &info)
 		Napi::Error::New(env, "3 arguments are required").ThrowAsJavaScriptException();
 	}
 
-	if (info[0].IsNull() || info[0].IsUndefined() || !info[0].IsUint8Array())
+	if (info[0].IsNull() || info[0].IsUndefined() || !info[0].IsTypedArray() || info[0].As<Napi::TypedArray>().TypedArrayType() != napi_uint8_array)
 	{
 		Napi::Error::New(env, "First argument, data, must be a valid Uint8Array").ThrowAsJavaScriptException();
 		return env.Null();
 	}
 
-	if (info[1].IsNull() || info[1].IsUndefined() || !info[1].IsUint8Array())
+	if (info[1].IsNull() || info[1].IsUndefined() || !info[1].IsTypedArray() || info[1].As<Napi::TypedArray>().TypedArrayType() != napi_uint8_array)
 	{
 		Napi::Error::New(env, "Second argument, optionalEntropy, must be null or an ArrayBuffer").ThrowAsJavaScriptException();
 		return env.Null();
 	}
 
-	if (info[2].IsNull() || info[2].IsUndefined() || !info[2].IsString())
+	if (info[2].IsNull() || info[2].IsUndefined() || !info[2].IsTypedArray() || info[2].As<Napi::TypedArray>().TypedArrayType() != napi_uint8_array)
 	{
 		Napi::Error::New(env, "Third argument, scope, must be a string").ThrowAsJavaScriptException();
 		return env.Null();
